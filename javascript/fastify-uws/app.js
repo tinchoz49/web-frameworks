6a572ecfa5203a24c37a542944965c7fcc287ba2
Martin Acosta <tinchoz49@gmail.com>
2024-03-22 12:10:58 -0300

    add @geut/fastify-uws

---
+++ javascript/fastify-uws/app.js
@@ -0,0 +1,20 @@
+const fastify = require('fastify');
+const { serverFactory } = require('@geut/fastify-uws');
+
+const app = fastify({
+  serverFactory
+});
+
+app.get('/', function (request, reply) {
+  reply.send();
+});
+
+app.get('/user/:id', function (request, reply) {
+  reply.send(request.params.id);
+});
+
+app.post('/user', function (request, reply) {
+  reply.send();
+});
+
+app.listen({ port: 3000, host: '0.0.0.0' }, function () {});
